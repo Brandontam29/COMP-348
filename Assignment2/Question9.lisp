0a58a276aa0934a35023ef66b943c86d137f9eeb
courvalv1428 <courvalv@gmail.com>
2021-11-05 19:25:42 -0400

    Update Question 9 works !!!!

--- Assignment2/Question9.lisp
+++ Assignment2/Question9.lisp
@@ -7,11 +7,32 @@
 
 (print "Question 9: In-Order and Pre-Order Tree")
 
+(defun hasLeft(lst)
+    (caadr lst)
+    )
+
+(defun hasRight (lst)
+    (caaddr lst)
+    )
+
+(defun in-order (lst)
+    (if (hasLeft lst) (in-order (car (cdr lst))))
+    (write (car lst))
+    (if (hasRight lst) (in-order (car (cdr (cdr lst))))))
+
+(defun pre-order (lst)
+    (write (car lst))
+    (if (hasLeft lst) (pre-order (car (cdr lst))))
+    (if (hasRight lst) (pre-order (car (cdr (cdr lst))))))
+
 
 
 
 (print "Testing the function with the examples in the assignment instructions: ")
-(print (in-order '(+ (- (1 () ()) (* (4 () ())(7 () ()))) (/ (7 () ()) (6 () ()))))
-(1 - 4 * 7 + 7 / 6))
-(print (pre-order '(+ (- (1 () ()) (* (4 () ())(7 () ()))) (/ (7 () ()) (6 () ()))))
-(+ - 1 * 4 7 / 7 6))
+(print "In-Order test")
+(in-order '(+ (- (1 () ()) (* (4 () ())(7 () ()))) (/ (7 () ()) (6 () ()))))
+;(1 - 4 * 7 + 7 / 6))
+(print "Pre-Order test")
+(pre-order '(+ (- (1 () ()) (* (4 () ())(7 () ()))) (/ (7 () ()) (6 () ()))))
+;(+ - 1 * 4 7 / 7 6))
+
