0bd3c68baeaa01591de1376fa849038be4dd70fb
Brandon Tam <brandon.tam29@gmail.com>
2021-12-07 23:41:42 -0500

    fixed some stuff

--- Assignment_03/Question4.py
+++ Assignment_03/Question4.py
@@ -17,35 +17,53 @@ class Multiset:
         self.multiset.remove(item)
 
     def count(self, item):
-        count = 0
-        for x in self.multiset:
-            if(x == item):
-                count += 1
-        return count
+        return self.multiset.count(item)
 
     def union(self, set1):
-        newSet = sorted(list(set(self.multiset)) + list(set(set1)))
-        return newSet
+        allValues = sorted(list(set(self.multiset + set1)))
+        s1 = self.multiset
+        s2 = set1
+        s3 = []
+        for x in allValues:
+            c1 = s1.count(x)
+            c2 = s2.count(x)
+            if c1 > c2:
+                for _ in range(c1):
+                    s3.append(x)
+            if c2 > c1:
+                for _ in range(c2):
+                    s3.append(x)
+
+        return s3
 
     def intersection(self, set1):
-        newSet = list(set(self.multiset) & set(set1))
-        return newSet
+        allValues = sorted(list(set(self.multiset + set1)))
+        s1 = self.multiset
+        s2 = set1
+        s3 = []
+        for x in allValues:
+            c1 = s1.count(x)
+            c2 = s2.count(x)
+            if c1 < c2:
+                for _ in range(c1):
+                    s3.append(x)
+            if c2 < c1:
+                for _ in range(c2):
+                    s3.append(x)
+
+        return s3
 
     def difference(self, set2):
         for x in set2:
             count = Multiset(set2).count(x)
             if count > 0:
-                for _ in range(count):
-                    if self.multiset.count(x) > 0:
-                        self.multiset.remove(x)
-                        break
-                    else:
-                        break
+                if self.multiset.count(x) > 0:
+                    self.multiset.remove(x)
 
 
 # Test cases
 m1 = Multiset([1, 2, 2])
-m2 = Multiset([1, 1, 2, 3])
+m2 = Multiset([1, 1, 2, 2, 2, 3])
 
 # Addition
 m1.add(6)
@@ -59,13 +77,13 @@ print(m1.multiset)  # [1, 2, 2]
 print(m1.count(2))  # 2
 
 # Union
-print(m1.union(m2.multiset))  # [1, 1, 2, 2, 3, 4]
+print(m1.union(m2.multiset))  # [1, 1, 2, 2, 2, 3]
 
 # Intersection
-print(m1.intersection(m2.multiset))  # [1, 2]
+print(m1.intersection(m2.multiset))  # [1, 2, 2]
 
 # Difference update
-m1.add(2)  # This checks for unsorted sets
+m1.add(2)  # This tests for unsorted sets
 m2.add(2)
 m1.difference(m2.multiset)
 print(m1.multiset)  # [2]
